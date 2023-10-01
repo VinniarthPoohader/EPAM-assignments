431df1e81e37ad943b02183c8f2ecb35a7e34f97
VinniarthPoohader <145115350+VinniarthPoohader@users.noreply.github.com>
2023-10-01 12:58:15 +0400

    Number Converter

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Lesson5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("В какую систему переводим (2-20)?");
+            int system = int.Parse(Console.ReadLine());
+
+            int remainder;
+            string result = string.Empty;
+            while (number > 0)
+            {
+                remainder = number % system;
+                number /= system;
+                result = remainder.ToString() + result;
+            }
+            Console.WriteLine("Получается:  {0}", result);
+
+        }
+    }
+}
+                
+            
+            
+            
